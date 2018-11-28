8d4d24c25941a8414c07df59eedbc42f2461479a
immokorolev <45413869+immokorolev@users.noreply.github.com>
2018-11-28 08:20:46 +0300

    Create script-git.js

---
+++ script-git.js
@@ -0,0 +1,59 @@
+<script>
+$(document).ready(function(){
+    $('.t-input-group_nm, .t-input-group_em, .t-input-group_ph').wrapAll('<div class="wrap-input" />');
+    $('.t-submit').append('<img class="circle" src="https://static.tildacdn.com/tild3135-3332-4661-b939-626433646437/_13.png" />');
+    $('.t-upwidget-container__button').append('<img class="attachment" src="https://static.tildacdn.com/tild6565-6561-4732-b734-613063356433/attachment.png" />');
+    $('.t-upwidget-container__button_fileholder').append('<img class="attachment" src="https://static.tildacdn.com/tild6565-6561-4732-b734-613063356433/attachment.png" />');
+    $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:first-child').click(function(){
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:first-child').addClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(2)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(3)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(4)').removeClass('t-radio__control-active');
+    });
+    $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(2)').click(function(){
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:first-child').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(2)').addClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(3)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(4)').removeClass('t-radio__control-active');
+    });
+    $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(3)').click(function(){
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:first-child').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(2)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(3)').addClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(4)').removeClass('t-radio__control-active');
+    });
+    $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(4)').click(function(){
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:first-child').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(2)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(3)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066473296"] .t-radio__control:nth-child(4)').addClass('t-radio__control-active');
+    });
+    
+    
+    $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:first-child').click(function(){
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:first-child').addClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(2)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(3)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(4)').removeClass('t-radio__control-active');
+    });
+    $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(2)').click(function(){
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:first-child').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(2)').addClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(3)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(4)').removeClass('t-radio__control-active');
+    });
+    $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(3)').click(function(){
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:first-child').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(2)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(3)').addClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(4)').removeClass('t-radio__control-active');
+    });
+    $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(4)').click(function(){
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:first-child').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(2)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(3)').removeClass('t-radio__control-active');
+        $('.t-input-group_rd[data-input-lid="1543066661627"] .t-radio__control:nth-child(4)').addClass('t-radio__control-active');
+    });
+
+});
+</script>
