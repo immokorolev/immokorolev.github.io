afec84f658ebe52c9a5375b1681e9dcf1b79cf41
immokorolev <45413869+immokorolev@users.noreply.github.com>
2018-11-28 08:44:58 +0300

    Update script-gl.js

--- script-gl.js
+++ script-gl.js
@@ -56,3 +56,51 @@ $(document).ready(function(){
     });
 
 });
+// Плавающее меню
+$(document).ready(function(){
+    var blockMenuID = '#rec76332169';
+    $(blockMenuID).addClass('floating');
+    $(blockMenuID).addClass('display');
+    $(window).scroll(function() { 
+        var top = $(document).scrollTop();
+        if (top >= 100) { 
+            $(blockMenuID).removeClass('display');
+            $(blockMenuID).addClass('fixed'); 
+        } else {
+            $(blockMenuID).addClass('display');
+            $(blockMenuID).removeClass('fixed');
+        }
+ });
+ // Слайдер "Наши клиенты"
+ $(document).ready(function(){
+    $(".t-slds__arrow-left").append('<img src="https://static.tildacdn.com/tild3964-3938-4532-b935-656536636337/arrow-left.png" class="nsocicon t-img" imgfield="img">');
+    $(".t-slds__arrow-right").append('<img src="https://static.tildacdn.com/tild3631-3236-4261-b939-313231303238/arrow-right.png" class="nsocicon t-img" imgfield="img">');
+});
+    
+// Блок "Наши проекты"
+$('.t-slds__bgimg[data-original="https://static.tildacdn.com/tild6464-3466-4465-b364-623139643162/6.jpg"]').wrap('<img class="circle" src="https://static.tildacdn.com/tild3135-3332-4661-b939-626433646437/_13.png" />');
+ $('[href = "#portfolio"]').click(function() {
+    $('.tn-elem__762439711542891032071').css('display','none');
+    $('.tn-elem__762439711542891032092').css('display','none');
+    $('.tn-elem__762439711542891032104').css('display','none');
+    $('.tn-elem__762439711542961326221').css('display','table');
+    $('.tn-elem__762439711543379189347').css('display','table');
+setTimeout(function() {
+   $('#rec76416076').slideDown(500);  
+},);
+}); 
+ });
+// Блок "Вакансии"
+$(document).ready(function(){
+    $('.tn-elem[data-elem-id="1542975740102"] .tn-atom').click(function (){
+        $('#rec76310600').css('display','block');
+        $('#rec76536875').css('display','none');
+        $('#rec76310600').css('transition','display 0.3s ease-in-out');
+    });
+    $('.tn-elem[data-elem-id="1542975887572"] .tn-atom').click(function (){
+        $('#rec76310600').css('display','none');
+        $('#rec76536875').css('display','block');
+        $('#rec76536875').css('transition','display 0.3s ease-in-out');
+    });
+});
+
